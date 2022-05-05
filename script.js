29a5cfba8387ca21f24cbf4d9d8f6c336e1fa24c
CoccoRandela <rocco.candela94@gmail.com>
2022-05-05 16:03:44 +0100

    Make positive/negative button available

--- script.js
+++ script.js
@@ -17,6 +17,15 @@ function divide (a, b) {
     else return a/b; 
 }
 
+function expo (a, b) {
+    let result = 1;
+    for (let i=0; i<b; i++) {
+        result *= a;
+    }
+    return result;
+}
+
+
 function operate (a, operator, b) {
     switch(operator) {
         case '+':
@@ -31,6 +40,9 @@ function operate (a, operator, b) {
         case '÷':
             return divide(a, b);
             break;
+        case 'ˆ':
+            return expo(a, b);
+            break;
     }
 }
 
@@ -38,7 +50,9 @@ function operate (a, operator, b) {
 const numberButtons = document.querySelectorAll('.number'),
 clearButton = document.querySelector('.clear-button'),
 operatorButtons = document.querySelectorAll('.operator'),
-equalButton = document.querySelector('.equal');
+squareButton = document.querySelector('.square'),
+equalButton = document.querySelector('.equal'),
+signButton = document.querySelector('.sign');
 
 const display = document.querySelector('.display'),
 operandDisplay = document.querySelector('.operand-display'),
@@ -48,29 +62,82 @@ let currentOperand = null,
 firstOperand = null,
 secondOperand = null,
 operator = null;
+result = null;
 
 operandDisplay.textContent = Number(currentOperand);
 
 
 numberButtons.forEach(b => {
     b.addEventListener('click', () => {
+        if (computationDisplay.textContent.includes('=')) {
+            computationDisplay.textContent =  currentOperand;
+        }
         computationDisplay.textContent += b.textContent;
         currentOperand = Number(Number(currentOperand) + b.textContent);
         operandDisplay.textContent = currentOperand;
-        if (operator) return secondOperand = currentOperand;
+        if (operator) secondOperand = currentOperand;
     })
 })
 
 operatorButtons.forEach(b => {
     b.addEventListener('click', () => {
+        if (computationDisplay.textContent.includes('=')) {
+            computationDisplay.textContent =  currentOperand;
+        }
         if (secondOperand || secondOperand === 0) {
             currentOperand = operate(firstOperand, operator, secondOperand);
             operandDisplay.textContent = currentOperand;
         }
         computationDisplay.textContent += b.textContent;
-        return firstOperand = currentOperand,
-        currentOperand = 0,
+        firstOperand = currentOperand;
+        currentOperand = null;
         operator = b.textContent;    
     })
 })
 
+clearButton.addEventListener('click', () => {
+    currentOperand = null;
+    operandDisplay.textContent = Number(currentOperand);
+    computationDisplay.textContent =  '';
+    firstOperand = null;
+    secondOperand = null;
+    operator = null;
+})
+
+equalButton.addEventListener('click', () => {
+    if (secondOperand || secondOperand === 0) {
+        let result = operate(firstOperand, operator, secondOperand);
+        operandDisplay.textContent = result; 
+        computationDisplay.textContent = computationDisplay.textContent + equalButton.textContent + result;
+        currentOperand = result;
+        firstOperand = null;
+        secondOperand = null;
+        operator = null;
+    }
+})
+
+signButton.addEventListener('click', () => {
+    if (currentOperand) {
+        if (currentOperand > 0) {
+            computationDisplay.textContent = computationDisplay.textContent.slice(0, -currentOperand.toString().length)
+            currentOperand = subtract(currentOperand, currentOperand*2);
+            operandDisplay.textContent = currentOperand;
+            computationDisplay.textContent += `(${currentOperand})`;
+        } else if (currentOperand < 0) {
+            computationDisplay.textContent = computationDisplay.textContent.slice(0, -currentOperand.toString().length-2)
+            currentOperand = subtract(currentOperand, currentOperand*2)
+            operandDisplay.textContent = currentOperand;
+            operandDisplay.textContent = currentOperand;
+            computationDisplay.textContent += currentOperand;
+        }
+    } else {
+        if (firstOperand > 0) {
+            firstOperand = subtract(firstOperand, firstOperand*2);
+            operandDisplay.textContent = firstOperand;
+        } else if (firstOperand < 0) {
+            firstOperand = subtract(currentOperand, currentOperand*2)
+            operandDisplay.textContent = firstOperand;
+        }
+    }
+    if (operator) secondOperand = currentOperand;
+})
